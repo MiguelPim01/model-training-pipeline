b3699cb88522f5aa1b852ff6f4e3f1bda65e627f
MiguelPim01 <miguelmip02@gmail.com>
2026-01-21 17:24:29 -0300

    refac: Refactoring the template class

--- src/templates/training_pipeline_template.py
+++ src/templates/training_pipeline_template.py
@@ -4,32 +4,6 @@ class TrainingPipelineTemplate(ABC):
     def __init__(self):
         pass
 
+    @abstractmethod
     def run(self):
-        self.preprocess_data()
-        self.load_dataset()
-        self.load_datamodel()
-        self.train_model()
-        self.evaluate_model()
-        self.deploy_model()
-    
-    @abstractmethod
-    def preprocess_data(self):
-        pass
-    
-    
-    def load_dataset(self):
-        pass
-    
-    
-    def load_datamodel(self):
-        pass
-    
-    @abstractmethod
-    def train_model(self):
-        pass
-    
-    def evaluate_model(self):
-        pass
-    
-    def deploy_model(self):
         pass
